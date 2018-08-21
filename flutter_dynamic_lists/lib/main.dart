30488d054a6cb39e9063b7bad8479fc6586de039
Tom Koptel <tom.koptel@gmail.com>
2018-08-21 21:09:09 +0300

    Implement Stream App

--- flutter_dynamic_lists/lib/main.dart
+++ flutter_dynamic_lists/lib/main.dart
@@ -1,109 +1,104 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(new MyApp());
+void main() {
+  return runApp(MaterialApp(
+    title: 'Photo Streamer',
+    theme: ThemeData(
+      primaryColor: Colors.green,
+    ),
+    home: PhotoList(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class PhotoList extends StatefulWidget {
+  @override
+  _PhotoListState createState() => _PhotoListState();
+}
+
+class _PhotoListState extends State<PhotoList> {
+  final String url = "https://jsonplaceholder.typicode.com/photos";
+  List<Photo> photos = [];
+  StreamController<Photo> streamController;
+
+  @override
+  void initState() {
+    super.initState();
+    print('_PhotoListState#initState()');
+    streamController = StreamController.broadcast();
+    streamController.stream
+        .listen((photo) => setState(() => photos.add(photo)));
+    load();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    streamController?.close();
+    streamController = null;
+  }
+
+  load() async {
+    var client = http.Client();
+    var request = http.Request('get', Uri.parse(url));
+    var response = await client.send(request);
+
+    response.stream
+        .transform(Utf8Decoder())
+        .transform(json.decoder)
+        .expand((e) => e)
+        .map((map) => Photo.fromJsonMap(map))
+        .pipe(streamController);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Streams'),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      body: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: photos.length,
+          itemBuilder: (_, int index) {
+            return _makeElement(index);
+          }),
+    );
+  }
+
+  Widget _makeElement(int index) {
+    if (index >= photos.length) {
+      return null;
+    }
+    var photo = photos[index];
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            photo.url,
+            height: 150.0,
+            width: 150.0,
+          ),
+          Text(photo.title)
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Photo {
   final String title;
+  final String url;
+
+  Photo.fromJsonMap(Map map)
+      : title = map['title'],
+        url = map['url'];
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  String toString() {
+    return "title: $title url: $url";
   }
 }
