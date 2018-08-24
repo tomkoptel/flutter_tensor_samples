9b7c8428182091a6d30684eab6f6f4eee6a1078a
Tom Koptel <tom.koptel@gmail.com>
2018-08-24 17:51:47 +0300

    Add animation for Flutter logo

---
+++ flutter_intro_animations/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+void main() => runApp(MaterialApp(
+      home: AnimSample(),
+    ));
+
+class AnimSample extends StatefulWidget {
+  @override
+  _AnimSampleState createState() => _AnimSampleState();
+}
+
+class _AnimSampleState extends State<AnimSample>
+    with SingleTickerProviderStateMixin {
+  Animation animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        duration: Duration(milliseconds: 3000), vsync: this);
+    animation = Tween(begin: 0.0, end: 500.0).animate(animationController);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('text'),
+      ),
+      body: LogoAnimation(animation: animation),
+    );
+  }
+}
+
+class LogoAnimation extends AnimatedWidget {
+  LogoAnimation({Key key, Animation animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation animation = listenable;
+    return Center(
+      child: Container(
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
