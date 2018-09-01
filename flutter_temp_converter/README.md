5245ce47edf9d15784d64e362b440b2977d42114
Tom Koptel <tom.koptel@gmail.com>
2018-09-01 17:06:34 +0300

    Add Calc app sample

---
+++ flutter_temp_converter/README.md
@@ -0,0 +1,8 @@
+# flutter_temp_converter
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ flutter_temp_converter/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme = ThemeData.light().copyWith(
+  accentColor: Colors.red,
+  toggleableActiveColor: Colors.green,
+  primaryColor: Colors.green,
+);
+
+void main() => runApp(MaterialApp(
+      theme: appTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculator App'),
+        ),
+        body: CalcApp(),
+      ),
+    ));
+
+class CalcApp extends StatefulWidget {
+  @override
+  _CalcAppState createState() => _CalcAppState();
+}
+
+class _CalcAppState extends State<CalcApp> {
+  double input;
+  double output;
+  bool fOrC;
+
+  @override
+  void initState() {
+    super.initState();
+    input = 0.0;
+    output = 0.0;
+    fOrC = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextField inputField = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          labelText: 'Input value in ${fOrC ? "Celsius" : "Fahrenheit"}'),
+      onChanged: (str) {
+        try {
+          input = double.parse(str);
+        } catch (e) {
+          input = 0.0;
+        }
+      },
+    );
+    Container tempSwitch = Container(
+      child: Column(
+        children: <Widget>[
+          Text('Choose Fahrenheit or Celsius'),
+          Row(
+            children: <Widget>[
+              Text("F"),
+              _fOrCradio(value: false),
+              Text("C"),
+              _fOrCradio(value: true),
+            ],
+          ),
+        ],
+      ),
+    );
+    Container calcBtn = Container(
+      child: RaisedButton(
+        child: Text('Calculate'),
+        onPressed: () {
+          fOrC
+              ? output = (input * 9 / 5) + 32
+              : output = (input - 32) * (5 / 9);
+
+          AlertDialog alertDialog = AlertDialog(
+            content: fOrC
+                ? Text(
+                    "${input.toStringAsFixed(3)} C: ${output.toStringAsFixed(3)} F")
+                : Text(
+                    "${input.toStringAsFixed(3)} F: ${output.toStringAsFixed(3)} C"),
+          );
+          showDialog(context: context, child: alertDialog);
+        },
+      ),
+    );
+
+    return Container(
+      padding: EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          children: <Widget>[inputField, tempSwitch, calcBtn],
+        ),
+      ),
+    );
+  }
+
+  Widget _fOrCradio({bool value = false}) {
+    return Radio<bool>(
+      groupValue: fOrC,
+      value: value,
+      onChanged: (bool newValue) {
+        setState(() {
+          fOrC = newValue;
+        });
+      },
+    );
+  }
+}
